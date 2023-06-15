079fccca4356b4c69b235570b2b342ef4d413acb
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-15 05:00:17 -0700

    0x00. C - Hello, World mandatory tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 
 /**
- * main - You can print a c program using printf function
- *return: 0 means the success of a program
+ * main - You can print a c program using printf functioin
+ * Return: 0 means the success of a program
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
