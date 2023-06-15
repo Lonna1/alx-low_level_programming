ce526beb079cd6c049d31a1d5629a23cf24ba762
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-15 06:28:37 -0700

    0x00. C - Hello, advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unstd.h>
+
+/**
+ * main - help to print a program using fput function
+ * Return: 1 indicates the successfulness of a program
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
