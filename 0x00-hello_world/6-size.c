0fb63604b59bd23634532adcfcd42a858bb44621
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-15 05:29:27 -0700

    0x00. C - Hello, World mandatory tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,20 @@
 #include<stdio.h>
 
 /**
- * main - help to print c program using printf function
- * Return : 0 means the successful of a program
+ * * main - help to print c program using printf function
+ * * Return : 0 means the successful of a program
 */
-
-int main()
+int main(void)
 {
-int i;
-char c;
-long int x;
-long long int y;
-float z;
-	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n",(unsigned long)sizeof(i));
-	printf("Size of a log int: %lu byte(s)\n",(unsigned long)sizeof(x));
-	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(y));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
-	return 0;
+	int i;
+	char c;
+	long int x;
+	long long int y;
+	float z;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a log int: %lu byte(s)\n", (unsigned long)sizeof(x));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
+return (0);
 }
