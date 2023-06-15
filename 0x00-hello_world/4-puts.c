ace16b4675d3e5470bfcf3bf3782972e6e3c3ac1
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-15 04:34:47 -0700

    0x00. C - Hello, World mandatory tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+ * main - this program use puts function to print a line
+ * Return: 0 means the success of a program
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
