13dc1b6df91662c49e9dcd0d8f4f0b49fb68f28b
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-15 06:03:21 -0700

    0x00. C - Hello, World mandatory tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * * main - help to print c program using printf function
- * * Return : 0 means the successful of a program
-*/
+ * main - help to print c program using printf function
+ * Return: 0 means the successful of a program
+ */
 int main(void)
 {
 	int i;
@@ -14,7 +14,7 @@ int main(void)
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(y));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
 return (0);
 }
