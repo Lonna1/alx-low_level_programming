8eb7847fe5af12fd78e5aa4482ad84dd99aef33f
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-15 05:31:35 -0700

    0x00. C - Hello, World mandatory tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float z;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a log int: %lu byte(s)\n", (unsigned long)sizeof(x));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
 return (0);
