6d74a2c3349daef1513a373881d48d234951ba15
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-16 07:38:16 -0700

    0x01. C - Variables, if, else, while advanced tasks

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main -This is the program Entry point
+ * Return: 0 indicates the successful of a program
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 99; x++)
+	{
+		for (y = x; y <= 99; y++)
+		{
+        int z= x /10;
+        int u= x % 10;
+        int v= y /10;
+        int w= y % 10;
+
+			putchar (z + '0');
+			putchar (u + '0');
+			putchar (' ');
+			putchar (v + '0');
+			putchar (w + '0');
+			if (x <= 98 || y <= 98)
+			{
+				putchar (',');
+				putchar (' ');
+			}}}
+	putchar ('\n');
+	return (0);
+}
