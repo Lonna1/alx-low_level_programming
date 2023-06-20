bd88f5d683c7b0f67329c4242274994ac306dce5
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-20 04:24:52 -0700

    0x02. C - Functions, nested loops Mandatory tasks

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
+#include "main.h"
+
 /**
- * main - Entry point of the program
- * print_alphabet - function name to be called by main
- * Return: 0 shows the successful of a program
+ * print_alphabet - function to print alphabets in lower cases
  */
+
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar (c);
