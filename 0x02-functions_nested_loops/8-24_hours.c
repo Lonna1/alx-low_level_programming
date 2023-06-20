b15c0d5a5ef4218db1393a866efe29b3fb4378ab
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-20 05:20:56 -0700

    0x02. C - Functions, nested loops Mandatory tasks

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,17 +7,17 @@
 void jack_bauer(void)
 {
 	int a, b;
-	
-	for (a = 0; a< 24; a++) 
-    {
-	    for (b = 0; b < 60; b++)
-	    {
-            _putchar('0' + a / 10);
-            _putchar('0' + a % 10);
-            putchar(':');
-            _putchar('0' + b / 10);
-            _putchar('0' + c % 10);
-            _putchar('\n');
-        }
-    }
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar('0' + a / 10);
+			_putchar('0' + a % 10);
+			_putchar(':');
+			_putchar('0' + b / 10);
+			_putchar('0' + c % 10);
+			_putchar('\n');
+		}
+	}
 }
