7c860f361f4536310b3568e62ea80e37033e8110
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-20 05:07:10 -0700

    0x02. C - Functions, nested loops Mandatory tasks

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _abs - This will help in computing the absolute value of a number
- * Return : n shows the successful of a program
- * Return : -n shows the successful of a program
+ * _abs - helps to get the absolute value of an integer
+ * @x: helps to check the absolute value
+ * Return: help to check the successfullness of a program
  */
-
 int _abs(int x)
 {
 	if (x < 0)
 	{
 		return (-x);
 	}
-	else(x >0)
+	else
 	{
 		return (x);
 	}
