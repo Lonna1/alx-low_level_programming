f5367c32af6723ec729356a5aedb250323475af6
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-20 07:54:22 -0700

    0x02. C - Functions, nested loops advanced tasks

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * Return: 0 shows the success of the program
+ */
+int main(void)
+{
+	unsigned long int x, y, z, u, v, w, a;
+
+	y = 1;
+	Z = 2;
+
+	printf("%lu", y);
+
+	for (x = 1; x <= 90; i++)
+	{
+		printf(", %lu", z);
+		z = z + y;
+		y = z - y;
+	}
+	u = y / 1000000000;
+	v = y % 1000000000;
+	w = z / 1000000000;
+	a = z % 1000000000;
+
+	for (x = 92; x <= 98; x++)
+	{
+		printf(", %lu", w + (a / 1000000000));
+		printf("%lu", a % 1000000000);
+		w = w + u;
+		u = w - u;
+		a = a + v;
+		v = a - v;
+	}
+	printf("\n");
+
+	return (0);
+}
