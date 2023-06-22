18bfda0860605bc4a49269ebd3b6adc0e7e479f7
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-22 05:13:38 -0700

    0x04. C - More functions, more nested loops Mandatory Tasks

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9,
- * except 2 and 4
+ * print_most_numbers - prints the numbers, from 0 to 9
  */
 void print_most_numbers(void)
 {
 	int a;
 
-	for (a = 0; a <= 9; i++)
+	for (a = 0; a < 10; i++)
 	{
 		if (a != 2 && a != 4)
 		{
