01c7a784d738c3e6f43bee6a27f8f12c3fd9701e
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-22 05:00:11 -0700

    0x04. C - More functions, more nested loops Mandatory Tasks

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - help to print numbers from 0 to 9
- * Return: alwasys shows the success of the program
  */
 
 void print_numbers(void)
@@ -14,5 +13,4 @@ void print_numbers(void)
 		_putchar (a + '0');
 	}
 	_putchar ('\n');
-	return (0);
 }
