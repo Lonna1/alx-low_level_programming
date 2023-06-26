6f09dbb5dfdbc7a352007453603ac5d6af4e9b07
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-26 10:31:58 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program
+ * Return: 0 to show the success of the program
+ */
+
+int main(void)
+{
+	int pass[100];
+	int i, addition, n;
+
+	addition = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		addition += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - addition) - '0' < 78)
+		{
+			n = 2772 - addition - '0';
+			addition += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
