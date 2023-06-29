7ce23e5eee09c6db337bac561c113ec9069da558
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-29 06:12:00 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - print ot magic numbers
- *
+ * main - print of magic numbers
  * Return: 0
  */
-
 int main(void)
 {
 	int n;
@@ -14,10 +11,9 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-
 	/*
 	 * write your line of code here...
-	 * Remember
+	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
 	 * - only one statement
