66d13caeee10a1ba8d2bce4ff3d9fbfbe31f47c1
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-29 04:57:56 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13
+ * @a: string to be encoded
+ * Return: a to show the successful of the program
+ */
+
+char *rot13(char *a)
+{
+	int b;
+	int c;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (b = 0; a[b] != '\0'; i++)
+	{
+		for (c = 0; c < 52; c++)
+		{
+			if (a[b] == data1[c])
+			{
+				a[b] = datarot[c];
+				break;
+			}
+		}
+	}
+	return (a);
+}
