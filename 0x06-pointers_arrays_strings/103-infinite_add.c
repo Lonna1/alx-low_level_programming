648a3a0203dce7827fed1b118b842b48ef699093
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-06-29 07:01:57 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,37 +11,37 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int o, p, q, u, s, t;
 
-	for (i = 0; n1[i]; i++)
+	for (o = 0; n1[o]; o++)
 		;
-	for (j = 0; n2[j]; j++)
+	for (p = 0; n2[p]; p++)
 		;
-	if (i > size_r || j > size_r)
+	if (o > size_r || p > size_r)
 		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	s = 0;
+	for (o -= 1, p -= 1, q = 0; q < size_r - 1; o--, p--, q++)
 	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
-		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
+		t = s;
+		if (o >= 0)
+			t += n1[o] - '0';
+		if (p >= 0)
+			t += n2[p] - '0';
+		if (o < 0 && p < 0 && t == 0)
 		{
 			break;
 		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
+		s = t / 10;
+		r[q] = t % 10 + '0';
 	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
+	r[q] = '\0';
+	if (o >= 0 || p >= 0 || s)
 		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
+	for (q -= 1, u = 0; u < q; q--, u++)
 	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
+		s = r[q];
+		r[q] = r[u];
+		r[u] = s;
 	}
 	return (r);
 
