02210f43f4ec7d04f9067cce80112b6a2e117d09
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-07-07 04:20:48 -0700

    ou mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - library to print factorial
+ * @n: number for factorial to be printed
+ * Return: -1 if n is lower than 0.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	}
+	return (n * factorial(n - 1));
+}
