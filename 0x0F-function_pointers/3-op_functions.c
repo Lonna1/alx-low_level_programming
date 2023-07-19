a9fe4437d31b3ae1b2736f2c92d20c0babba44cb
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-07-19 05:03:04 -0700

    a program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+/**
+ * op_add - fucntion that adds @a and @b
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: sum of inserted integers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - fucntion that retrieves the difference of @a and @b
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: difference of both integers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that multiplies @a and @b
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: product of the integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that divides @a by @b
+ * @a: 1st uinteger
+ * @b: 2nd integer
+ * Return: results of the division @a and @b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function that retrieve the reminder
+ * of the division between @a and @b
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: the remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
