731b15918f12a764c44895ed0c90c241d3c44c26
Blaise Hatangimana <blaise.hatangimana1@gmail.com>
2023-08-07 06:19:39 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,10 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int df, result = 1, countn = 0;
+	int df;
+	ssize_t _write;
+	int size;
+	char *mem;
 
 	if (!filename)
 	{
@@ -21,16 +24,44 @@ int create_file(const char *filename, char *text_content)
 	df = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (df == -1)
 		return (-1);
-	if (text_content)
+	if (!text_content)
 	{
-		while (text_content[countn])
-			countn++;
-		result = write(df, text_content, countn);
-	}
-	if (result == -1)
-	{
-		return (-1);
 		close(df);
+		return (1);
 	}
-	return(1);
+	size = _strlen(text_content);
+	mem = malloc(sizeof(char) * size);
+	if (!mem)
+	{
+		close(df);
+		return (-1);
+	}
+	_write = write(df, text_content, size);
+	if (_write == -1)
+	{
+		close(df);
+		free(mem);
+		return (-1);
+	}
+	close(df);
+	free(mem);
+	return (1);
+}
+
+/**
+ * _strlen - string length function
+ * @s: a pointer to a character
+ * Return: 0 for the program successfulness
+ */
+
+int _strlen(const char *s)
+{
+	int x = 0;
+
+	while (*(s + x) != '\0')
+	{
+		x++;
+	}
+
+	return (x);
 }
